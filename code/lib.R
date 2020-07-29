5b30a2a31e5cab5a59dcb0c85d40b44fea072a00
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2020-07-29 20:26:51 -0300

    Explorando os dados brutos de felipe de 20200729

---
+++ code/lib.R
@@ -0,0 +1,19 @@
+
+import_data <- function(){
+    d = read_delim(here::here("data/raw/dados do ismir/ranking - t090 - w10 -  warmup.txt"), 
+                   delim = "|", 
+                   col_names = F)
+    d2 = d %>% 
+        mutate(X2 = str_trim(X2)) %>% 
+        separate(X2, sep = "-", into = c("year", "artist", "year2", "album"), remove = F) %>% 
+        mutate(year = as.integer(str_remove_all(year, "[\\[\\]]")), 
+               data = str_extract(X3, "(?<=\\[).+?(?=\\])")) %>% 
+        separate(data, sep = ",", into = c("i", "j", "k", "D")) %>% 
+        select(X1, X2, year, artist, album, song = X3, i, j, k, D)
+    
+    
+}
+
+read_projectdata <- function(){
+
+}
